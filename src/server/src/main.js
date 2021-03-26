ee576ba380fefa18b820015018263114f786e69b
Raefaldhi Amartya Junior <scythx@tutanota.com>
2021-03-26 07:19:53 +0700

    Update main.js

--- src/server/src/main.js
+++ src/server/src/main.js
@@ -2,9 +2,9 @@ import db from './db'
 import server from './server'
 
 db.authenticate().then(() => {
-  console.log('Database connection has been established successfully.')
+console.log('Database connection has been established successfully.')
 
-  server.listen(process.env.SERVER_PORT, () =>
+server.listen(process.env.SERVER_PORT, () =>
     console.log(`Server app listening on port ${process.env.SERVER_PORT}!`)
   )
 }).catch((error) => {
