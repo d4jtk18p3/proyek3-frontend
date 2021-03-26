375a8a0c04789820159532131def699a4b6d9193
M.Andhika <48423397+andhika276@users.noreply.github.com>
2021-03-26 14:28:03 +0700

    Update 20210320083619-create-user.cjs

--- src/server/resources/migrations/20210320083619-create-user.cjs
+++ src/server/resources/migrations/20210320083619-create-user.cjs
@@ -1,7 +1,30 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-	await queryInterface.dropTable('Users'); 
+	await queryInterface.dropTable('Users');
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
     await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
