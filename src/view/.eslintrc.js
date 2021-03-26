b64b1dbc61ba7aa010cea826b88f807f3b52f1fd
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-03-26 08:10:20 +0700

    Delete redundant file

--- src/view/.eslintrc.js
+++ src/view/.eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/essential",
+    "@vue/standard"
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'quotes': ["error","double",{"allowTemplateLiterals" : true}]
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["error", "double", { allowTemplateLiterals: true }]
   }
 }
--- src/view/src/http.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_SERVER_BASE_URL,
-  withCredentials: true
-})
-
-export default instance
