2e1bb4eba15afbc41f91f79fd2a8b3bc40da8689
Deonetwo <dewantoade11@gmail.com>
2021-04-05 20:13:36 +0700

    [view] Menambah template navbar dan sidebar

---
+++ src/view/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <SideBar/>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./views/pages/home/HelloWorld"
+import SideBar from "./views/component/UI/SideBar"
+
+export default {
+  name: "App",
+  props: {
+    propsExample: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return value.toString() !== `Dani`
+      }
+    }
+  },
+  components: {
+    HelloWorld,
+    SideBar
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
