192ef1775207386f1be7e3be7d85d71117dd3907
Deonetwo <dewantoade11@gmail.com>
2021-04-05 20:13:36 +0700

    [view] Menambah template navbar dan sidebar

---
+++ src/view/src/views/component/UI/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-navigation-drawer
+      absolute
+      bottom
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  components: {
+  }
+}
+</script>
