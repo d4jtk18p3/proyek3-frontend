3d47946e796e72da1f3e61c0e72d0dcaf103b363
zefangracio <48423324+zefangracio@users.noreply.github.com>
2021-04-08 22:55:06 +0700

    [view] menambah component navbar

--- src/view/src/views/component/UI/NavBar.vue
+++ src/view/src/views/component/UI/NavBar.vue
@@ -1,41 +1,28 @@
 <template>
+<v-card class="overflow-hidden">
     <v-app-bar
-      app
-      color="primary"
-      dark
+      absolute
+      color="white"
+      scroll-target="#scrolling-techniques-7"
       clipped-left
     >
-      <div class="d-flex align-center ">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-img :src="require('../../../assets/polban.png')" contain height="40" />
+      <v-btn icon>
+        <v-container link>
+        <v-icon color="purple">mdi-account-circle-outline</v-icon>
+        <v-icon color="purple">mdi-menu-down</v-icon>
+        </v-container>
       </v-btn>
     </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-7"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1500px;">
+      </v-container>
+    </v-sheet>
+  </v-card>
 </template>
 
 <script>
