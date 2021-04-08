c2e05a27d6fce41eb4e10b77b7097467e8e305e4
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-04-08 21:49:23 +0700

    Keycloak setup done

--- src/view/src/datasource/http.js
+++ src/view/src/datasource/http.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const NETWORK_TIMEOUT = 10000
-
+const AUTH_URL = "http://127.0.0.1:8080/"
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_SERVER_BASE_URL,
+  baseURL: AUTH_URL,
   withCredentials: true,
   timeout: NETWORK_TIMEOUT
 })
