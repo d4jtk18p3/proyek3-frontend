1bfe7e1254f531f0a237e4a88daa39a3830588d9
Dwinanda <dwinanda.alfauzan.tif418@polban.ac.id>
2021-04-11 15:08:33 +0700

    [server] create table migration

---
+++ src/server/src/models/akun.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+const Akun = sequelize.define("akun", {
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+    tipe_akun: {
+        type: Sequelize.STRING
+    }
+});
+
+module.export = Akun;
