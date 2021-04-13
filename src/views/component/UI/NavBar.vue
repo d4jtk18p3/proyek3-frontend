f7d074bd78db8a63aff3f591a13b5ed4ff32cad7
Mohamad Naufal Fadhil <naufalfadhil12344@gmail.com>
2021-04-13 08:17:00 +0700

    Feat : Navbar , Sidebar , Keycloak Integration, Color Theming (#14)
    * Keycloak setup done
    
    * [view] Menambah template navbar dan sidebar
    
    * [view] Membuat component sidebar
    
    * merge sidebar
    
    * [view] menambah component navbar
    
    * add top bar
    
    bug - sheet makan tempat buat content
    
    * merge theming
    
    * [view] Memperbaiki bugs pada navbar
    
    * [view] Memperbaiki posisi navbar menjadi fixed
    
    * Delete view
    
    * change port
    
    * Update theme.js
    
    * Update package-lock.json
    
    * Fix Lint
    
    Co-authored-by: Deonetwo <dewantoade11@gmail.com>
    Co-authored-by: zefangracio <48423324+zefangracio@users.noreply.github.com>
    Co-authored-by: Icha 
    Co-authored-by : Nadhillah

---
+++ src/views/component/UI/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app-bar
+      app
+      color="white"
+      clipped-left
+      fixed
+    >
+      <v-img :src="require('../../../assets/polban.png')" contain height="40" absolute class="ml-4"/>
+      <v-btn :elevation="0" width="10" class="white" absolute right>
+        <v-container>
+        <v-icon color="white" style="background: purple; background-size: 50%; border-radius: 100%;">mdi-account-circle</v-icon>
+        <v-icon color="purple">mdi-menu-down</v-icon>
+        </v-container>
+      </v-btn>
+    </v-app-bar>
+</template>
+<style>
+</style>
+<script>
+export default {
+  name: "NavBar",
+  components: {
+  }
+}
+</script>
