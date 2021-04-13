d2f5e2d142fc0869ae58f023d00c64a9cba295e6
Mohamad Naufal Fadhil <naufalfadhil12344@gmail.com>
2021-04-13 21:06:29 +0700

    Done (#16)

---
+++ src/views/component/UI/Breadcumbs.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-breadcrumbs
+    class="pa-0"
+    :items="breadcrumbItems"
+  >
+    <template v-slot:divider>
+      <div :style="{color: isDark ? currentTheme.onBackground : 'C4C4C4'}">&gt;</div>
+    </template>
+    <template v-slot:item="{item}">
+      <v-breadcrumbs-item
+        :disabled="item.disabled"
+      >
+        <div
+          class="font-weight-bold"
+          :style="item.disabled ? breadcrumbItemDisableStyle : breadcrumbItemEnableStyle"
+        >{{item.text}}</div>
+      </v-breadcrumbs-item>
+    </template>
+  </v-breadcrumbs>
+</template>
+<script>
+import { mapGetters } from "vuex"
+export default {
+  name: "breadcumbs",
+  props: {
+    breadcrumbItems: {
+      type: Array,
+      required: false,
+      default: () => {
+        return [
+          {
+            text: "Absensi",
+            disabled: false,
+            href: ""
+          },
+          {
+            text: "Link 1",
+            disabled: false,
+            href: ""
+          },
+          {
+            text: "Link 2",
+            disabled: true,
+            href: ""
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    ...mapGetters({
+      currentTheme: "theme/getCurrentColor",
+      isDark: "theme/getIsDark"
+    }),
+    breadcrumbItemEnableStyle () {
+      return {
+        color: this.isDark ? this.currentTheme.colorSecondary : this.currentTheme.onBackground
+      }
+    },
+    breadcrumbItemDisableStyle () {
+      return {
+        color: this.isDark ? this.currentTheme.onBackground : "#C4C4C4"
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/component/absensi/KelasItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    :style="{background: '#FB8C00'}"
+    class="pa-3"
+  >
+    <div
+      class="text-center text-h6"
+      :style="{color: currentTheme.onSurface}"
+    >3A - D4 Teknik Informatika</div>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  name: "KelasItem",
+  computed: {
+    ...mapGetters({
+      currentTheme: "theme/getCurrentColor"
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
