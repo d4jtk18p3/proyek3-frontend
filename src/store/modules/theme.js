2e0dbca323e7b84d4e63793deccdc43a3fa66876
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-04-13 08:12:27 +0700

    Fix Lint

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -1,60 +1,60 @@
 // const CURRENT_THEME_KEY = "CURRENT_THEME_KEY_STORAGE"
 
 const Light = {
-    colorPrimary : "#272343",
-    colorPrimaryVariant : "#272343",
-    colorSecondary : "#59DCDC",
-    colorSecondaryVariant : "#E3F6F5",
-    colorOnPrimary : "#FFFFF",
-    colorOnSecondary : "#272343",
-    background : "#FFFFFF",
-    surface : "#FFFFFF",
-    onBackground : "#272343",
-    onSurface : "#272343",
-    onError : "#FF5252"
+  colorPrimary: "#272343",
+  colorPrimaryVariant: "#272343",
+  colorSecondary: "#59DCDC",
+  colorSecondaryVariant: "#E3F6F5",
+  colorOnPrimary: "#FFFFF",
+  colorOnSecondary: "#272343",
+  background: "#FFFFFF",
+  surface: "#FFFFFF",
+  onBackground: "#272343",
+  onSurface: "#272343",
+  onError: "#FF5252"
 }
 
 const Dark = {
-    colorPrimary : "#272343",
-    colorPrimaryVariant : "#272343",
-    colorSecondary : "#59DCDC",
-    colorSecondaryVariant : "#E3F6F5",
-    colorOnPrimary : "#FFFFFF",
-    colorOnSecondary : "#272343",
-    background : "#181A1D",
-    surface : "#262626",
-    onBackground : "#FFFFFF",
-    onSurface : "#FFFFF",
-    onError : "#FF5252"
+  colorPrimary: "#272343",
+  colorPrimaryVariant: "#272343",
+  colorSecondary: "#59DCDC",
+  colorSecondaryVariant: "#E3F6F5",
+  colorOnPrimary: "#FFFFFF",
+  colorOnSecondary: "#272343",
+  background: "#181A1D",
+  surface: "#262626",
+  onBackground: "#FFFFFF",
+  onSurface: "#FFFFF",
+  onError: "#FF5252"
 }
 
 const ThemeModule = {
-    namespaced: true,
-    state: () => ({
-        currentColor :Light,
-        isDark : false
-    }),
-    mutations :{
-        SET_IS_DARK(state , isDark){
-            state.isDark = isDark;
-            if(isDark){
-                state.currentColor = Dark
-            }else{
-                state.currentColor = Light
-            }
-        },
-    },
-    getters : {
-        getCurrentColor(state){
-            return state.currentColor;
-        },
-        getIsDark(state){
-            return state.isDark;
-        }
-    },
-    actions : {
-
+  namespaced: true,
+  state: () => ({
+    currentColor: Light,
+    isDark: false
+  }),
+  mutations: {
+    SET_IS_DARK (state, isDark) {
+      state.isDark = isDark
+      if (isDark) {
+        state.currentColor = Dark
+      } else {
+        state.currentColor = Light
+      }
     }
+  },
+  getters: {
+    getCurrentColor (state) {
+      return state.currentColor
+    },
+    getIsDark (state) {
+      return state.isDark
+    }
+  },
+  actions: {
+
+  }
 
 }
 
