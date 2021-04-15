bc62a2179471dc9d36d0e4719067173b259c7dce
Ananda Bayu <anandabayu090@gmail.com>
2021-04-15 17:53:32 +0700

    Update TugasItem.vue
    * add rounded
    * fix dark/light mode

--- src/views/component/dashboard/TugasItem.vue
+++ src/views/component/dashboard/TugasItem.vue
@@ -1,13 +1,13 @@
 <template>
   <v-card
-    class="rounded-card"
+    class="rounded-card rounded-lg"
     elevation="10"
   >
     <v-row class="pa-4 ma-0" :style="{background: '#FB8C00'}">
       <v-col cols="12" align-self="center" class="pa-0 ma-0">
         <div
           class="text-h5"
-          :style="{color : currentTheme.surface}"
+          :style="{color : isDark ? currentTheme.onSurface : currentTheme.surface}"
         >Tugas</div>
       </v-col>
     </v-row>
