d8cf62d4f5c4cad29a8a70afcd8442bcbe81393f
Ananda Bayu <anandabayu090@gmail.com>
2021-05-18 17:36:21 +0700

    Create LihatLogbook.vue
    * halaman detail logbook (mahasiswa-view)

---
+++ src/views/logbook/pages/mahasiswa/LihatLogbook.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-row :style="{color: currentTheme.onBackground}">
+    <v-col cols="12">
+      <p class="text-h4 font-weight-bold">View Logbook Mahasiswa</p>
+    </v-col>
+    <v-col cols="12">
+      <breadcumbs :breadcrumb-items="breadcrumbItems"/>
+    </v-col>
+    <v-col :cols="isMobile ? `12` : `5`">
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-row class="py-5 ml-3">
+            <v-col :cols="isMobile ? '4' : '3'" class="py-0">
+              <div class="text-start">Nama</div>
+            </v-col>
+            <v-col :cols="isMobile ? '7' : '5'" class="py-0">
+              <div class="text-start">: {{identitas.nama}}</div>
+            </v-col>
+            <v-col :cols="isMobile ? '1' : '4'"></v-col>
+            <v-col :cols="isMobile ? '4' : '3'" class="py-0">
+              <div class="text-start">NIM</div>
+            </v-col>
+            <v-col :cols="isMobile ? '7' : '5'" class="py-0">
+              <div class="text-start">: {{identitas.nim}}</div>
+            </v-col>
+              <v-col :cols="isMobile ? '1' : '4'"></v-col>
+            <v-col :cols="isMobile ? '4' : '3'" class="py-0">
+              <div class="text-start">Kelas</div>
+            </v-col>
+            <v-col :cols="isMobile ? '7' : '5'" class="py-0">
+              <div class="text-start">: {{identitas.kelas}}</div>
+            </v-col>
+              <v-col :cols="isMobile ? '1' : '4'"></v-col>
+            <v-col :cols="isMobile ? '4' : '3'" class="py-0">
+              <div class="text-start">Prodi</div>
+            </v-col>
+            <v-col :cols="isMobile ? '7' : '7'" class="py-0">
+              <div class="text-start">: {{identitas.prodi}}</div>
+            </v-col>
+            <v-col :cols="isMobile ? '1' : '2'"></v-col>
+            <v-col :cols="isMobile ? '4' : '3'" class="py-0">
+              <div class="text-start">Mata Kuliah</div>
+            </v-col>
+            <v-col :cols="isMobile ? '7' : '5'" class="py-0">
+              <div class="text-start">: {{identitas.matakuliah}}</div>
+            </v-col>
+            <v-col :cols="isMobile ? '1' : '4'"></v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-1">
+          <DatePickerItem
+          :picker="this.logbookdate"
+          :viewOnly="true"/>
+        </v-col>
+
+      </v-row>
+    </v-col>
+    <v-col :cols="isMobile ? `12` : `6` ">
+      <ViewDetailLogbookItem
+      :kegiatan="this.kegiatan"
+      :hasil="this.hasil"
+      :kesan="this.kesan"/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import Breadcumbs from "@/views/shared/navigation/Breadcumbs"
+import DatePickerItem from "@/views/logbook/component/mahasiswa/DatePickerItem"
+import ViewDetailLogbookItem from "@/views/logbook/component/mahasiswa/ViewDetailLogbookItem"
+
+export default {
+  name: "LihatLogbook",
+  components: { DatePickerItem, Breadcumbs, ViewDetailLogbookItem },
+  props: {
+    identitas: {
+      type: Object,
+      required: false,
+      default: () => {
+        return {
+          nama: "Cecep Gorbacep",
+          nim: "181524000",
+          kelas: "3A",
+          prodi: "D4 - Teknik Informatika",
+          matakuliah: "Proyek 3"
+        }
+      }
+    },
+    kegiatan: {
+      type: String,
+      required: false,
+      default: () => {
+        return ""
+      }
+    },
+    hasil: {
+      type: String,
+      required: false,
+      default: () => {
+        return ""
+      }
+    },
+    kesan: {
+      type: String,
+      required: false,
+      default: () => {
+        return ""
+      }
+    },
+    logbookdate: {
+      type: Date,
+      required: false,
+      default: () => {
+        return new Date().toISOString().substr(0, 10)
+      }
+    }
+  },
+  data () {
+    return {
+      breadcrumbItems: [
+        {
+          text: "Logbook",
+          disabled: false,
+          href: ""
+        },
+        {
+          text: "Logbook Saya",
+          disabled: false,
+          href: "/logbook/mylogbook"
+        },
+        {
+          text: "Lihat Logbook",
+          disabled: true,
+          href: ""
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters({
+      currentTheme: "theme/getCurrentColor"
+    }),
+    isMobile () {
+      return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs
+    }
+  }
+}
+</script>
