3a65248a5503dd8e18058dff1c7b9739a85f3aa4
Ananda Bayu <anandabayu090@gmail.com>
2021-05-18 17:27:24 +0700

    Update TabelItem.vue
    * menambahkan icon view, edit, delete di setiap row
    * text truncate untuk kolom kegiatan, hasil, dan kesan
    * menambahkan fungsional button view, edit, dan delete
    * menambahkan dialog konfirmasi hapus, hapus berhasil, dan hapus gagal

--- src/views/logbook/component/mahasiswa/TabelItem.vue
+++ src/views/logbook/component/mahasiswa/TabelItem.vue
@@ -10,7 +10,24 @@
         dark
         mobile-breakpoint="100"
     >
-      <template v-slot:item.editbutton="{ item }">
+      <template v-slot:[`item.kegiatan`]="{ item }">
+        <div
+        class="text-truncate"
+        style="max-width: 50px;">
+          {{item.kegiatan}}
+        </div>
+      </template>
+
+      <template v-slot:[`item.viewbutton`]="{ item }">
+        <v-btn
+          icon
+          color="#FFFFFF"
+        >
+          <v-icon>mdi-eye</v-icon>
+        </v-btn>{{item.viewbutton}}
+      </template>
+
+      <template v-slot:[`item.editbutton`]="{ item }">
         <v-btn
           icon
           color="#FFFFFF"
@@ -19,7 +36,7 @@
         </v-btn>{{item.editbutton}}
       </template>
 
-      <template v-slot:item.deletebutton="{ item }">
+      <template v-slot:[`item.deletebutton`]="{ item }">
         <v-btn
           icon
           color="#FFFFFF"
@@ -37,24 +54,159 @@
         light
         mobile-breakpoint="100"
     >
-      <template v-slot:item.editbutton="{ item }">
+      <template v-slot:[`item.kegiatan`]="{ item }">
+        <div
+        class="text-truncate"
+        style="max-width: 200px;">
+          {{item.kegiatan}}
+        </div>
+      </template>
+
+      <template v-slot:[`item.hasil`]="{ item }">
+        <div
+        class="text-truncate"
+        style="max-width: 200px;">
+          {{item.hasil}}
+        </div>
+      </template>
+
+      <template v-slot:[`item.kesan`]="{ item }">
+        <div
+        class="text-truncate"
+        style="max-width: 200px;">
+          {{item.kesan}}
+        </div>
+      </template>
+
+      <template v-slot:[`item.viewbutton`]="{ item }">
         <v-btn
           icon
-          color="#000000"
+          color="#272343"
+          @click="onViewClick(item.tanggal, item.kegiatan, item.hasil, item.kesan)"
+        >
+          <v-icon>mdi-eye</v-icon>
+        </v-btn>{{item.viewbutton}}
+      </template>
+
+      <template v-slot:[`item.editbutton`]="{ item }">
+        <v-btn
+          icon
+          color="#272343"
+          @click="onEditClick(item.tanggal, item.kegiatan, item.hasil, item.kesan)"
         >
           <v-icon>mdi-pencil-outline</v-icon>
         </v-btn>{{item.editbutton}}
       </template>
 
-      <template v-slot:item.deletebutton="{ item }">
+      <template v-slot:[`item.deletebutton`]="{ item }">
         <v-btn
           icon
-          color="#000000"
+          color="#272343"
+          @click.stop="openConfirmDialog(item)"
         >
           <v-icon>mdi-delete-sweep-outline</v-icon>
         </v-btn>{{item.deletebutton}}
       </template>
     </v-data-table>
+
+    <v-dialog
+      v-model="confirmdialog"
+      width="500"
+      persistent
+    >
+      <v-card
+      :dark="isDark">
+        <v-card-title class="headline font-weight-light">
+          Apakah anda ingin menghapus data logbook ini?
+        </v-card-title>
+
+        <v-card-text>
+          data yang akan dihapus yaitu data logbook tanggal <strong>{{this.deleteddatatanggal}}</strong>. Data yang telah dihapus tidak dapat dikembalikan.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            outlined
+            color="error"
+            @click="closeConfirmDialog"
+          >
+            tidak
+          </v-btn>
+          <v-btn
+            outlined
+            :color="isDark ? currentTheme.onSurface:'#272343'"
+            @click="openDialog"
+          >
+            ya
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="successdialog"
+      width="500"
+      persistent
+    >
+      <v-card
+      :dark="isDark">
+        <v-card-title class="headline font-weight-light"
+        :class="currentTheme.colorPrimary">
+          Logbook Berhasil Dihapus
+        </v-card-title>
+
+        <v-card-text>
+
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            outlined
+            :color="isDark ? currentTheme.onSurface:'#272343'"
+            @click="closeSuccessDialog"
+          >
+            ok
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="errordialog"
+      width="500"
+      persistent
+    >
+      <v-card
+      :dark="isDark">
+        <v-card-title class="headline font-weight-light red--text"
+        :class="currentTheme.colorPrimary">
+          Logbook Gagal Dihapus
+        </v-card-title>
+
+        <v-card-text>
+
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            outlined
+            :color="isDark ? currentTheme.onSurface:'#272343'"
+            @click="closeErrorDialog"
+          >
+            ok
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -70,84 +222,84 @@ export default {
         return [
           {
             nomor: "1",
-            tanggal: "19 April 2021",
-            kegiatan: "Membuat design halaman utama log...",
+            tanggal: "01-03-2021",
+            kegiatan: "Membuat design halaman utama log Membuat design halaman utama log",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "2",
-            tanggal: "19 April 2021",
+            tanggal: "02-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "3",
-            tanggal: "19 April 2021",
+            tanggal: "03-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "4",
-            tanggal: "19 April 2021",
+            tanggal: "04-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "5",
-            tanggal: "19 April 2021",
+            tanggal: "05-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "6",
-            tanggal: "19 April 2021",
+            tanggal: "06-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "7",
-            tanggal: "19 April 2021",
+            tanggal: "07-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "8",
-            tanggal: "19 April 2021",
+            tanggal: "08-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "9",
-            tanggal: "19 April 2021",
+            tanggal: "09-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "10",
-            tanggal: "19 April 2021",
+            tanggal: "10-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "11",
-            tanggal: "19 April 2021",
+            tanggal: "11-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
           },
           {
             nomor: "12",
-            tanggal: "19 April 2021",
+            tanggal: "12-03-2021",
             kegiatan: "Membuat design halaman utama log...",
             hasil: "Hasil design telah dikumpulkan di fig...",
             kesan: "Ketika mendesign dapat pengalaman..."
@@ -172,8 +324,7 @@ export default {
         {
           text: "Kegiatan",
           align: "start",
-          value: "kegiatan",
-          class: "text-truncate"
+          value: "kegiatan"
         },
         {
           text: "Hasil",
@@ -189,6 +340,12 @@ export default {
           text: "",
           align: "center",
           sortable: false,
+          value: "viewbutton"
+        },
+        {
+          text: "",
+          align: "center",
+          sortable: false,
           value: "editbutton"
         },
         {
@@ -197,7 +354,13 @@ export default {
           sortable: false,
           value: "deletebutton"
         }
-      ]
+      ],
+      confirmdialog: false,
+      successdialog: false,
+      errordialog: false,
+      delete: true,
+      deleteddata: null,
+      deleteddatatanggal: ""
     }
   },
   computed: {
@@ -207,6 +370,69 @@ export default {
     }),
     isMobile () {
       return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs
+    }
+  },
+  methods: {
+    onEditClick (tanggal, kegiatan, hasil, kesan) {
+      var parts = tanggal.split("-")
+      var date = new Date(parts[2], parts[1] - 1, parts[0], 7).toISOString().substr(0, 10)
+      alert(date)
+      this.$router.push({
+        name: "EditLogbook",
+        path: "/logbook/editlogbook/" + date,
+        params: {
+          logbookdate: date,
+          kegiatan: kegiatan,
+          hasil: hasil,
+          kesan: kesan
+        }
+      })
+      this.$router.go(1)
+    },
+    onViewClick (tanggal, kegiatan, hasil, kesan) {
+      var parts = tanggal.split("-")
+      var date = new Date(parts[2], parts[1] - 1, parts[0], 7).toISOString().substr(0, 10)
+      alert(date)
+      this.$router.push({
+        name: "ViewLogbook",
+        path: "/logbook/viewlogbook/" + date,
+        params: {
+          logbookdate: date,
+          kegiatan: kegiatan,
+          hasil: hasil,
+          kesan: kesan
+        }
+      })
+      this.$router.go(1)
+    },
+    openConfirmDialog (data) {
+      this.deleteddata = data
+      this.deleteddatatanggal = this.deleteddata.tanggal
+      this.confirmdialog = true
+    },
+    closeConfirmDialog () {
+      this.confirmdialog = false
+    },
+    openSuccessDialog () {
+      this.confirmdialog = false
+      this.successdialog = true
+    },
+    closeSuccessDialog () {
+      this.successdialog = false
+    },
+    openErrorDialog () {
+      this.confirmdialog = false
+      this.errordialog = true
+    },
+    closeErrorDialog () {
+      this.errordialog = false
+    },
+    openDialog () {
+      if (!this.delete) {
+        this.openErrorDialog()
+      } else {
+        this.openSuccessDialog()
+      }
     }
   }
 }
