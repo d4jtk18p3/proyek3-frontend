385de6f216e83cc21aea4dc4d80b5b1220eedeed
Raefaldhi Amartya Junior <scythx@tutanota.com>
2021-05-30 18:34:18 +0700

    Menambahkan alur masuk melalui vue frontend (#34)

---
+++ src/views/akun/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
