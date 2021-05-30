69a3f545fe3dae2868865225d44909e7571a8003
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-05-31 06:57:59 +0700

    Update ListAkun.vue

--- src/views/admin/pages/ListAkun/ListAkun.vue
+++ src/views/admin/pages/ListAkun/ListAkun.vue
@@ -1,18 +1,21 @@
 <template>
-    <v-data-table
-        dark
-        :headers="headers"
-        :items="items"
-        :loading="isLoading"
-        loading-text=""
-        :items-per-page="5"
-        class="elevation-1"
-        :style="{backgroundColor: currentTheme.colorPrimary}"
-    >
+  <v-data-table
+    dark
+    :headers="headers"
+    :items="items"
+    :loading="isLoading"
+    loading-text=""
+    :items-per-page="5"
+    class="elevation-1"
+    :style="{ backgroundColor: currentTheme.colorPrimary }"
+  >
     //<template v-slot:[`item.actions`]="{ item }">
-        //<v-icon small class="mr-2" @click="editAkun(item.id)">mdi-pencil</v-icon>
-    //</template>
-    </v-data-table>
+      //<v-icon small class="mr-2" @click="editAkun(item.id)"
+        >mdi-pencil</v-icon
+      >
+      //</template
+    >
+  </v-data-table>
 </template>
 
 <script>
@@ -26,20 +29,20 @@ export default {
       headers: [
         {
           text: "NIP/NIM",
-          value: "nomorInduk",
+          value: "attributes.noInduk[0]",
           sortable: false,
           class: "white--text text-lg-subtitle-1 font-weight-bold"
         },
-        {
-          text: "Jenis Nomor Induk",
-          value: "jenisNomorInduk",
-          class: "white--text text-lg-subtitle-1 font-weight-bold"
-        },
-        {
-          text: "Nama",
-          value: "nama",
-          class: "white--text text-lg-subtitle-1 font-weight-bold"
-        },
+        // {
+        //   text: "Jenis Nomor Induk",
+        //   value: "jenisNomorInduk",
+        //   class: "white--text text-lg-subtitle-1 font-weight-bold"
+        // },
+        // {
+        //   text: "Nama",
+        //   value: "nama",
+        //   class: "white--text text-lg-subtitle-1 font-weight-bold"
+        // },
         {
           text: "E-mail",
           value: "email",
@@ -47,7 +50,12 @@ export default {
         },
         {
           text: "Role/Jenis Akun",
-          value: "role",
+          value: "attributes.role[0]",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Status",
+          value: "attributes.isActive[0]",
           class: "white--text text-lg-subtitle-1 font-weight-bold"
         }
       ],
@@ -65,7 +73,7 @@ export default {
           console.log(e)
         }) */
       try {
-        const result = await axios.get("http://localhost:5001/user/")
+        const result = await axios.get("http://localhost:5001/user")
         this.items = result.data.data
         console.log(result.data.data)
       } catch (error) {
@@ -87,12 +95,12 @@ export default {
 
 <style lang="scss">
 tbody {
-    tr:hover {
-      background-color: #b9b9b9 !important;
-    }
-    tr {
-      color: black;
-      background-color: #FFFFFF !important;
-    }
+  tr:hover {
+    background-color: #b9b9b9 !important;
   }
+  tr {
+    color: black;
+    background-color: #ffffff !important;
+  }
+}
 </style>
