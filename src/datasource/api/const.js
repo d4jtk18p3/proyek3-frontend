6b14979580e360b7a7ba73316b3ad1e40e548b08
Ananda Bayu <anandabayu090@gmail.com>
2021-05-30 17:30:57 +0700

    Create const.js
    * nambahin koneksi rute ke backend

---
+++ src/datasource/api/const.js
@@ -0,0 +1,10 @@
+const BASE_URL = "http://localhost:5001"
+
+const LOGBOOKS_URL = BASE_URL + "/logbook/logbook"
+const LOGBOOKENTRI_URL = BASE_URL + "/logbook/entri"
+
+export {
+  BASE_URL,
+  LOGBOOKS_URL,
+  LOGBOOKENTRI_URL
+}
