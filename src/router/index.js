e5578b512cb7143d6a654747e72e9443dab87773
Raefaldhi Amartya Junior <scythx@tutanota.com>
2021-05-30 17:37:06 +0700

    Menambahkan alur masuk melalui vue frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
+import axios from "axios"
+import * as Keycloak from "keycloak-js"
 import Vue from "vue"
 import VueRouter from "vue-router"
+// import _ from "lodash"
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: "/akun",
+    meta: { requiresAuth: true },
+    component: () => import(/* webpackChunkName: "akun" */"../views/akun/Home"),
+    children: [
+      {
+        path: "masuk",
+        meta: { requiresAuth: false },
+        component: () => import(/* webpackChunkName: "akun.masuk" */"../views/akun/Masuk")
+      }
+    ]
+  },
   {
     path: "/template",
     component: () => import("../views/template/TemplateMain"),
@@ -128,10 +143,86 @@ const routes = [
   }
 ]
 
+// const fixup = (r) => {
+//   if (_.isArray(r)) {
+//     return _.map(r, route => {
+//       return fixup(route)
+//     })
+//   }
+
+//   if (_.has(r, "children")) {
+//     r.children = fixup(r.children)
+//   }
+//   r.path = r.path.replace(/^\/akun/, "/")
+
+//   return r
+// }
+
+// const fixup2 = (routes) => {
+//   return fixup(_.filter(routes, route => {
+//     return route.path.startsWith("/akun")
+//   }))
+// }
+
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  try {
+    if (!(to.meta.requiresAuth && from === VueRouter.START_LOCATION)) {
+      next()
+      return
+    }
+
+    await axios.post("https://akun.6766998f.nip.io/keycloak-proxy/configure", {
+      loginPattern: "/auth/realms/staging/protocol/openid-connect/auth",
+      loginUrl: "http://localhost:5002/akun/masuk"
+    }, {
+      withCredentials: true
+    })
+
+    const keycloak = Keycloak({
+      url: "https://akun.6766998f.nip.io/keycloak-proxy/auth",
+      realm: "staging",
+      clientId: "public"
+    })
+    const auth = await keycloak.init({ onLoad: "login-required" })
+
+    if (!auth) {
+      window.location.reload()
+    } else {
+      next()
+    }
+
+    setInterval(async () => {
+      try {
+        await keycloak.updateToken(70)
+      } catch (err) {
+        console.error(err)
+      }
+    }, 6000)
+  } catch (err) {
+    console.error(err)
+    next()
+  }
+})
+
+// router.beforeEach((to, from, next) => {
+//   if (from === VueRouter.START_LOCATION) {
+//     return next()
+//   }
+
+//   const newTo = Object.assign({}, to)
+//   newTo.path = newTo.path.replace(/^\/akun/, "/")
+
+//   if (to.path === newTo.path) {
+//     return next()
+//   }
+
+//   next(newTo)
+// })
 
 export default router
