50d6207d710d4a294b52b163be651c7b9883907a
Ananda Bayu <anandabayu090@gmail.com>
2021-05-30 17:33:29 +0700

    Create logbook.js
    * add fungsi logbooks satu mhs
    * add fungsi get id logbooks
    * add fungsi add logbooks

---
+++ src/datasource/api/logbook/logbook.js
@@ -0,0 +1,47 @@
+import axios from "axios"
+import { LOGBOOKS_URL } from "../const"
+import entri from "../logbook/entri"
+
+const getLogbooksMhsByNIM = async (nim) => {
+  try {
+    const urlGetIdLogbooks = LOGBOOKS_URL + `/${nim}`
+    const idLogbooks = await axios.get(urlGetIdLogbooks, {}, {})
+    console.log(idLogbooks.data.entri)
+    return entri.getAllEntryLogbookMhsByid(idLogbooks.data.entri)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const getIdLogbooksMhsByNIM = async (nim) => {
+  try {
+    const urlGetIdLogbooks = LOGBOOKS_URL + `/${nim}`
+    const idLogbooks = await axios.get(urlGetIdLogbooks, {}, {})
+    console.log(idLogbooks.data._id)
+    return idLogbooks.data._id
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const addLogbooksMhs = async (dataMhs) => {
+  try {
+    const urlAddLogbooks = LOGBOOKS_URL + `/create`
+    const result = await axios.post(urlAddLogbooks, {
+      nama: dataMhs.nama,
+      nim: dataMhs.nim,
+      kode_kelas: dataMhs.kode_kelas,
+      kelas_proyek: dataMhs.kelas_proyek
+    }, {})
+    console.log(result.data)
+    return result.data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export default {
+  getLogbooksMhsByNIM,
+  getIdLogbooksMhsByNIM,
+  addLogbooksMhs
+}
