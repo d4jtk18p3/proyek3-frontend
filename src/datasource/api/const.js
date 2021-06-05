6cfe8c6392fe746836331074e1761a989cf0768c
Ananda Bayu <anandabayu090@gmail.com>
2021-06-05 16:45:35 +0700

    Create const.js
    * add baseurl for logbook (dosen)

---
+++ src/datasource/api/const.js
@@ -0,0 +1,16 @@
+const BASE_URL = "http://localhost:5001"
+
+const MATAKULIAH_URL = BASE_URL + "/mata-kuliah"
+const PERKULIAHAN_URL = BASE_URL + "/perkuliahan"
+const KELAS_URL = BASE_URL + "/kelas"
+const LOGBOOKS_URL = BASE_URL + "/logbook/logbook"
+const LOGBOOKENTRI_URL = BASE_URL + "/logbook/entri"
+
+export {
+  BASE_URL,
+  MATAKULIAH_URL,
+  PERKULIAHAN_URL,
+  KELAS_URL,
+  LOGBOOKS_URL,
+  LOGBOOKENTRI_URL
+}
