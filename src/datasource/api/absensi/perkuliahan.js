7879b2aac7882d1455da50db124801dba9b81323
nadiamelyani <ndmlni591@gmail.com>
2021-07-02 08:40:15 +0700

    Update perkuliahan.js

--- src/datasource/api/absensi/perkuliahan.js
+++ src/datasource/api/absensi/perkuliahan.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-import { PERKULIAHAN_DOSEN } from "../const"
+import { PRESENSI_MHS_URL } from "../const"
 
 const getKehadiranMhs = async (kelas, idJadwal, tanggal) => {
   try {
-    const statusKehadiranUrl = PERKULIAHAN_DOSEN + `/kelas-jadwal?kodeKelas=${kelas}&idJadwal=${idJadwal}&tanggal=${tanggal}`
+    const statusKehadiranUrl = PRESENSI_MHS_URL + `/kelas-jadwal?kodeKelas=${kelas}&idJadwal=${idJadwal}&tanggal=${tanggal}`
     const result = await axios.get(statusKehadiranUrl)
     return result.data
   } catch (err) {
@@ -12,7 +12,7 @@ const getKehadiranMhs = async (kelas, idJadwal, tanggal) => {
 }
 const updateKehadiranMhs = async (idStudi, idJadwal, tanggal, isHadir) => {
   try {
-    const statusKehadiranUrl = PERKULIAHAN_DOSEN + `/update-kehadiran?idStudi=${idStudi}&idJadwal=${idJadwal}&tanggal=${tanggal}&isHadir=${isHadir}`
+    const statusKehadiranUrl = PRESENSI_MHS_URL + `/update-kehadiran?idStudi=${idStudi}&idJadwal=${idJadwal}&tanggal=${tanggal}&isHadir=${isHadir}`
     const result = await axios.put(statusKehadiranUrl)
     return result.data
   } catch (err) {
