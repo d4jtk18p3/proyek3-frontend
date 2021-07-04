0a340a0ff5e2e16983a85c8c7ad868a8f9b98ab3
Ananda Bayu <anandabayu090@gmail.com>
2021-07-04 20:03:44 +0700

    Implementasi halaman list matkul dan kelas (new)

--- src/views/logbook/pages/dosen/ListMataKuliah.vue
+++ src/views/logbook/pages/dosen/ListMataKuliah.vue
@@ -6,15 +6,55 @@
     <v-col cols="12">
         <breadcumbs :breadcrumb-items="breadcrumbItems"/>
     </v-col>
-    <v-col cols="12">
+    <v-col cols="3">
         <p
-          class="text-center font-weight-bold text-h5"
+          class="text-left font-weight-bold text-h5"
           :style="{color: currentTheme.onBackground}"
-        >Mata Kuliah</p>
+        >Dashboard</p>
+        <v-row>
+          <v-col :cols="isMobile ? 12 : 12">
+            <DashboardCardItem />
+          </v-col>
+          <v-col :cols="isMobile ? 12 : 10">
+            <p
+              class="text-left font-weight-bold text-h5"
+              :style="{color: currentTheme.onBackground}"
+            >Kelas</p>
+          </v-col>
+          <v-col :cols="isMobile ? 12 : 12" v-for="(item, index) in this.mappingKelasdanMatkul" :key="index">
+            <v-card
+              class="rounded-lg elevation-5"
+              @click="onKelasClicked(item.kelas)"
+            >
+              <v-row class="pa-4 ma-0" style="background: #FB8C00">
+                <v-col cols="12" align-self="center" class="pa-0 ma-0">
+                  <div
+                    class="text-subtitle-1 text-center font-weight-bold"
+                    :style="{color : currentTheme.onBackground}"
+                  >{{ item.kelas + ' - ' + item.matkul.prodi }} </div>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
     </v-col>
-    <v-col :cols="isMobile ? 12 : 4" v-for="(item, index) in listMataKuliah" :key="index">
-        <MataKuliahItem :mataKuliah="item.namaMataKuliah" :prodi="item.prodi" :listKelas="item.listKelas"/>
+    <v-col cols="0">
+      <v-divider
+        vertical
+      ></v-divider>
     </v-col>
+    <v-col cols="8">
+      <p
+        class="text-left font-weight-bold text-h5"
+        :style="{color: currentTheme.onBackground}"
+      >Mata Kuliah</p>
+      <v-row>
+        <v-col :cols="isMobile ? 12 : 5" v-for="(item, index) in this.listmatkul" :key="index">
+          <MataKuliahItem :kode="item.matkul.kode" :mataKuliah="item.matkul.namaMataKuliah" :prodi="item.matkul.prodi" :semester="item.matkul.semester" :kelasString="item.kelas"/>
+        </v-col>
+      </v-row>
+    </v-col>
+
   </v-row>
 </template>
 
@@ -22,29 +62,22 @@
 import { mapGetters, mapActions } from "vuex"
 import Breadcumbs from "@/views/shared/navigation/Breadcumbs"
 import MataKuliahItem from "@/views/logbook/component/dosen/MataKuliahItem"
+import DashboardCardItem from "../../component/dosen/DashboardCardItem"
 import BackEndMatakuliah from "../../../../datasource/network/logbook/matakuliah"
 import BackEndPerkuliahan from "../../../../datasource/network/logbook/perkuliahan"
 import BackEndKelas from "../../../../datasource/network/logbook/kelas"
 
 export default {
   name: "LogbookMahasiswa",
-  components: { Breadcumbs, MataKuliahItem },
+  components: { Breadcumbs, MataKuliahItem, DashboardCardItem },
   props: {
-    listMataKuliah: {
-      type: Array,
-      required: false,
-      default: () => {
-        return []
-      }
-    },
     dosen: {
       type: Object,
       required: false,
       default: () => {
         return {
-          nip: 1822310,
-          nama_dosen: "Ferry Feirizal",
-          id_jabatan: "Pengampu"
+          nip: "198903252019032000",
+          nama_dosen: "Sri Ratna Wulan"
         }
       }
     }
@@ -62,7 +95,10 @@ export default {
           disabled: true,
           href: ""
         }
-      ]
+      ],
+      mappingKelasdanMatkul: [],
+      listmatkul: [],
+      kelasChoosen: null
     }
   },
   computed: {
@@ -77,7 +113,44 @@ export default {
   methods: {
     ...mapActions({
       setNip: "logbook/setNip"
-    })
+    }),
+    kodeKelasToStringKelas (kodekelas) {
+      var date = new Date()
+      var year = date.getFullYear()
+
+      var kelasAngkaString = year.toString().substr(2, 4) // 2021 -> 21
+      var kelasAngkaStringInput = kodekelas.toString().substr(0, 2) // 1803 -> 18
+
+      var kelasAngka = parseInt(kelasAngkaString) - parseInt(kelasAngkaStringInput) // 21 - 18 = 3
+      kelasAngkaString = kelasAngka.toString()
+
+      var kelasHurufString = kodekelas.toString().substr(2, 4) // 1803 -> 03
+
+      if (kelasHurufString === "01" || kelasHurufString === "03") {
+        return kelasAngkaString + "A"
+      } else {
+        return kelasAngkaString + "B"
+      }
+    },
+    kodeProdiToStringProdi (kodeprodi) {
+      if (kodeprodi === "24") {
+        return "D4 Teknik Informatika"
+      } else if (kodeprodi === "11") {
+        return "D3 Teknik Informatika"
+      }
+    },
+    onKelasClicked (kelas) {
+      this.listmatkul = []
+      var i = 0
+      for (i = 0; i < this.mappingKelasdanMatkul.length; i++) {
+        if (kelas === this.mappingKelasdanMatkul[i].kelas) {
+          this.listmatkul.push({
+            kelas: kelas,
+            matkul: this.mappingKelasdanMatkul[i].matkul
+          })
+        }
+      }
+    }
   },
   async mounted () {
     this.setNip({
@@ -94,15 +167,18 @@ export default {
       var j = 0
       var kelasString = []
       while (j < kelas.length) {
-        kelasString[j] = kelas[j].kode_kelas
+        kelasString[j] = this.kodeKelasToStringKelas(kelas[j].kode_kelas)
+        this.mappingKelasdanMatkul.push({
+          kelas: kelasString[j],
+          matkul: {
+            kode: mataKuliahItem[0].id,
+            namaMataKuliah: mataKuliahItem[0].nama_mata_kuliah.substr(0, 8),
+            prodi: this.kodeProdiToStringProdi(mataKuliahItem[0].kode_program_studi),
+            semester: mataKuliahItem[0].semester
+          }
+        })
         j++
       }
-
-      this.listMataKuliah.push({
-        namaMataKuliah: mataKuliahItem[0].nama_mata_kuliah.substr(0, 8),
-        prodi: mataKuliahItem[0].kode_program_studi.substr(0, 2) + " - Teknik Informatika",
-        listKelas: kelasString
-      })
       i++
     }
   }
