019fb83697e2237662e195088ddfc93cf75d466a
nadiamelyani <48423383+nadiamelyani@users.noreply.github.com>
2021-07-27 12:33:29 +0700

    fix route

--- src/datasource/network/absensi/const.js
+++ src/datasource/network/absensi/const.js
@@ -1,10 +1,10 @@
 import { ABSENSI_API_URL } from "../../../config"
 
-const JADWAL_URL = new URL("/api/jadwal-perkuliahan", ABSENSI_API_URL).href
-const JADWAL_DOSEN_URL = new URL("/api/jadwal-perkuliahan", ABSENSI_API_URL).href
-const KETERANGAN_URL = new URL("/api/keterangan", ABSENSI_API_URL).href
-const PRESENSI_MHS_URL = new URL("/api/daftar-hadir-mahasiswa", ABSENSI_API_URL).href
-const PRESENSI_DSN_URL = new URL("/api/daftar-hadir-dosen", ABSENSI_API_URL).href
+const JADWAL_URL = new URL("/api/api/jadwal-perkuliahan", ABSENSI_API_URL).href
+const JADWAL_DOSEN_URL = new URL("/api/api/jadwal-perkuliahan", ABSENSI_API_URL).href
+const KETERANGAN_URL = new URL("/api/api/keterangan", ABSENSI_API_URL).href
+const PRESENSI_MHS_URL = new URL("/api/api/daftar-hadir-mahasiswa", ABSENSI_API_URL).href
+const PRESENSI_DSN_URL = new URL("/api/api/daftar-hadir-dosen", ABSENSI_API_URL).href
 
 export {
   JADWAL_URL,
