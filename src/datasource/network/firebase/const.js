aab2e8a3c1eac545e9729e00379c5a2091c22ceb
Raefaldhi Amartya Junior <raefaldhi.amartya.tif418@polban.ac.id>
2021-07-27 09:46:04 +0700

    Fix hardcode

--- src/datasource/network/firebase/const.js
+++ src/datasource/network/firebase/const.js
@@ -1,9 +1,11 @@
+import { NOTIFICATION_API_URL } from "../../../config"
+
 const NETWORK_TIMEOUT = 10000
-const BASE_URL = "http://localhost:5000"
-const GRUP_URL = BASE_URL + "/grup"
-const USER_DEVICE_URL = BASE_URL + "/user-device"
-const EMAIL_NOTIF_URL = BASE_URL + "/email-notif"
-const USER_URL = BASE_URL + "/user"
+const BASE_URL = NOTIFICATION_API_URL
+const GRUP_URL = new URL("/grup", NOTIFICATION_API_URL).href
+const USER_DEVICE_URL = new URL("/user-device", NOTIFICATION_API_URL).href
+const EMAIL_NOTIF_URL = new URL("/email-notif", NOTIFICATION_API_URL).href
+const USER_URL = new URL("/user", NOTIFICATION_API_URL).href
 
 export {
   NETWORK_TIMEOUT,
